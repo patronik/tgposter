5f845479daefe9ca16c74dee36603902a9ce9cbc
Vasyl Voina <vasyl.voina@gmail.com>
2025-12-20 13:21:17 +0200

    Allow duplicate items

--- src/renderer.js
+++ src/renderer.js
@@ -77,11 +77,7 @@ async function load() {
 }
 
 async function add() {
-  const id = document.getElementById('id').value;
-  const item = await window.api.getItem(id);
-  if (item) {
-    return;
-  }
+  const id = document.getElementById('id').value;  
   const comment = document.getElementById('comment').value;
   const reaction = document.getElementById('reaction').value;
   const prompt = document.getElementById('prompt').value;
